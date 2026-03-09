4c3227bdfebaa2dcec37d48ecc2489c5c2130616
Marcus Griffith-Jones <marcus.griffith-jones@tafesa.edu.au>
2026-03-09 15:18:25 +1030

    committed changes w an added main menu

---
+++ UniversalCalculator/MainMenu.xaml.cs
@@ -0,0 +1,23 @@
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Calculator
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainMenu : Page
+	{
+		public MainMenu()
+		{
+			InitializeComponent();
+		}
+	}
+}
